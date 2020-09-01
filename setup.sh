e1d3d2232c336091bcd8d2a7aa25beefe178fa1f
ilya1391 <65591144+ilya1391@users.noreply.github.com>
2020-09-01 22:04:22 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,1 @@
-pkg install python
-apt upgrade
-apt install pip
-pip install --upgrade pip
-pip install colorama
-pip install requests
-python pro.py
+apt install python && apt upgrade && apt install pip && pip install --upgrade pip && pip install colorama && pip install requests && python pro.py
